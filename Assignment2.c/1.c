e0986d0ce010a4d46bfa2d268b59ae903c52c689
manikiran949 <119949905+manikiran949@users.noreply.github.com>
2023-01-04 18:59:49 +0530

    Create 1.c

---
+++ Assignment2.c/1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    printf ("Escape characters in c\n");
+    printf("\\a : Alarm or beep\n");
+    printf("\\b : Backspace\n");
+    printf("\\f : Form Feed\n");
+    printf("\\n : New line\n");
+    printf("\\r : Carriage return\n"); 
+    printf("\\t : Tab(Horizontal)\n");
+    printf("\\v : Vertical Tab\n");
+    printf("\\\\ : Backslash\n");
+    printf("\\' : Single Quote\n");
+    printf("\\""\" : Double Quote\n");
+    printf("\\? : Question mark\n");
+    printf("\\ooo : octal number\n");
+    printf("\\0 : Null\n");
+    printf("%% : percentage sign\n");
+    return 0;
+}
